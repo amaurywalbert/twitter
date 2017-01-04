6c587f51ff0e3a01723cf75dfc52624e8b4373f0
amaurywalbert <amaurywalbert@yahoo.com.br>
2017-01-04 15:13:47 -0200

    atualizando arquivo de coleta dos egos

---
+++ ego_collect/EgoCollect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+INTERVALO=300 #5 minutos
+while true; do
+	# Verifica se o python está sendo executado
+	if pgrep -x "python" > /dev/null
+	then
+		echo "Executando"
+	else
+		echo "Iniciando..."
+		gnome-terminal -x bash -c "python /home/amaury/twitter/ego_collect/ego_collect_v5.py; exec $SHELL";
+	fi
+	sleep $INTERVALO
+done
