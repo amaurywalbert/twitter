120ffded288d787fbc50f57cfac13a77dbd9ecb5
amaurywalbert <amaurywalbert@yahoo.com.br>
2017-03-12 10:21:04 -0300

    Atualizando arquivo de coleta de egos

--- Collect.sh
+++ Collect.sh
@@ -1,13 +1,13 @@
 !/bin/bash
 INTERVALO=300	#5 minutos
-while true; do
-	# Verifica se o python está sendo executado
-	if pgrep -x "python" > /dev/null
-	then
-		echo "Executando"
-	else
-		echo "Iniciando..."
-		gnome-terminal -x bash -c "python ~/twitter/n1/n1_egos_friends_collect_v3.3.py; exec $SHELL";
-	fi
-	sleep $INTERVALO
-done
+#while true; do
+#	# Verifica se o python está sendo executado
+#	if pgrep -x "python" > /dev/null
+#	then
+#		echo "Executando"
+#	else
+#		echo "Iniciando..."
+#		gnome-terminal -x bash -c "python ~/twitter/n1/n1_egos_friends_collect_v3.3.py; exec $SHELL";
+#	fi
+#	sleep $INTERVALO
+#done
