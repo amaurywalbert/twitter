e563c919542864e95770e4e791aa3a86efc6fecf
amaurywalbert <amaurywalbert@yahoo.com.br>
2017-04-22 15:39:47 -0300

    Scripts de organização dos arquivos - N2

--- testes/full_egos/select_timeline_to_full_egos.py
+++ testes/full_egos/select_timeline_to_full_egos.py
@@ -8,7 +8,7 @@ import tweepy, datetime, sys, time, json, os, os.path, shutil, time, struct, ran
 reload(sys)
 sys.setdefaultencoding('utf-8')
 ######################################################################################################################################################################
-##		Status - Versão 1 - Para cada um dos 50 egos coletados, verifica na pasta dos timeline coletados  (10.0000) e copia o arquivo para a pasta timeline/50/
+##		Status - Versão 1 - Para cada um dos full egos, verifica na pasta dos favoritos coletados  (10.0000) e copia o arquivo para a pasta favorites/50/
 ##									Esse processo é apenas para agilizar e organizar os diretórios de favoritos já coletados.
 ## 
 ######################################################################################################################################################################
@@ -22,17 +22,22 @@ sys.setdefaultencoding('utf-8')
 ######################################################################################################################################################################
 
 def main():
-	i=0
-	for file in os.listdir(egos_friends_dir):					# Verifica a lista de egos coletados e para cada um, busca os amigos dos alters listados no arquivo do ego.
+	for file in os.listdir(egos_set):
 		user = file.split(".dat")
 		user = long(user[0])
-		try:
-			if os.path.isfile(timeline_collected+str(user)+".json"):
-				shutil.copy(timeline_collected+str(user)+".json",egos_50_timeline)
-				i+=1
-				print ("Arquivo copiado com sucesso! "+str(i))
-		except Exception as e:
-			print (e)
+		if not dictionary.has_key(user):
+			print ("Usuário ainda não coletado. Verificando no diretório 'coleta_old'. Ego:"+str(user))
+			try:
+				if os.path.isfile(timeline_collected+str(user)+".json"):
+					print ("Arquivo localizado...")
+					shutil.copy(timeline_collected+str(user)+".json",egos_timeline)
+					print ("Arquivo copiado com sucesso!\n")
+					print("######################################################################")
+				else:
+					print ("Arquivo NÃO encontrado!!!\n")
+					print("######################################################################")
+			except Exception as e:
+				print (e)
 
 ######################################################################################################################################################################
 #
@@ -43,14 +48,25 @@ def main():
 ################################### CONFIGURAR AS LINHAS A SEGUIR ####################################################
 ######################################################################################################################
 
-egos_friends_dir = "/home/amaury/coleta/n1/egos_friends/full/bin/"
+egos_set = "/home/amaury/coleta/n1/egos_friends/full/bin"
 
 timeline_collected = "/home/amaury/coleta_old/timeline_collect/10mil_egos/json/"
-egos_50_timeline = "/home/amaury/coleta/timeline_collect/full/json/"	
+egos_timeline = "/home/amaury/coleta/timeline_collect/full/json/"	
 
-#Cria os diretórios para armazenamento dos arquivos
-if not os.path.exists(egos_50_timeline):
-	os.makedirs(egos_50_timeline)
+
+dictionary = {}				#################################################### Tabela {chave:valor} para facilitar a consulta dos usuários já coletados
+###### Iniciando dicionário - tabela hash a partir dos arquivos já criados.
+print
+print("######################################################################")
+print ("Criando tabela hash...")
+i = 0	#Conta quantos usuários já foram coletados (todos arquivos no diretório)
+for file in os.listdir(egos_timeline):
+	user_id = file.split(".json")
+	user_id = long(user_id[0])
+	dictionary[user_id] = user_id
+	i+=1
+print ("Tabela hash criada com sucesso...") 
+print("######################################################################\n")
 
 #Executa o método main
 if __name__ == "__main__": main()
