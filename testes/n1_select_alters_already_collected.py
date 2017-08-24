cc2ceb956c0bd77f726929394907432697e20ded
amaurywalbert <amaurywalbert@yahoo.com.br>
2017-08-24 17:25:49 -0300

    Atualizando scripts de copia dos alters

--- testes/n1_select_alters_already_collected.py
+++ testes/n1_select_alters_already_collected.py
@@ -38,8 +38,11 @@ def main():
 	i = 0		# Ego verificado	
 	j = 0		# Quantidade de arquivos copiados 
 	k = 0		# Quantidade de arquivos não encontrados
-
+	
 	for file in os.listdir(fonte):
+		n_alters_missing = 0		# Quantidade de arquivos não encontrados por ego	
+		ego_id = file.split(".dat")
+		ego_id = long(ego_id[0])
 		alters_list = read_arq_bin(fonte+file) # Função para converter o binário de volta em string em formato json.
 		if alters_list:
 			for alter in alters_list:				
@@ -51,9 +54,19 @@ def main():
 						j+=1
 #						print (str(j)+" - Arquivo copiado com sucesso!")						
 					else:
+						n_alters_missing+=1		# Quantidade de arquivos não encontrados por ego
 						k+=1
 		i+=1
-		print ("Ego nº: "+str(i)+" - Verificado!")								
+		print ("Ego nº: "+str(i)+" - Verificado!")
+		egos_overview={}
+		with open(egos_output, "a+") as outfile:								# Abre o arquivo para gravação no final do arquivo
+			egos_overview = {'ego':ego_id,'n_alters_missing':n_alters_missing}
+			outfile.write(json.dumps(egos_overview,separators=(',', ':'))+"\n")								
+
+	with open(full_output, "a+") as outfile:								# Abre o arquivo para gravação
+		full_overview = {'files_in_dir':len(dictionary),'missing_files':k}
+		outfile.write(json.dumps(full_overview, separators=(',', ':')))
+
 	print ("Arquivos copiados: "+str(j))
 	print ("Arquivos no diretório: "+str(len(dictionary)))
 	print ("Arquivos faltando: "+str(k))
@@ -73,6 +86,9 @@ origem = "/home/amaury/coleta/n1/alters_friends/full/bin/"
 
 destino = "/home/amaury/dataset/n1/alters/bin/"
 
+egos_output = "/home/amaury/dataset/n2/alters/egos_overview.json"
+full_output = "/home/amaury/dataset/n2/alters/full_overview.json"
+
 formato = 'l'				################################################### Long para id do amigo
 user_struct = struct.Struct(formato) ###################################### Inicializa o objeto do tipo struct para poder armazenar o formato específico no arquivo binário
 
@@ -81,6 +97,9 @@ user_struct = struct.Struct(formato) ###################################### Inic
 if not os.path.exists(destino):
 	os.makedirs(destino)
 
+if os.path.isfile(egos_output):
+	os.remove(egos_output)
+	
 ###### Iniciando dicionário - tabela hash a partir dos arquivos já criados.
 print
 print("######################################################################")
