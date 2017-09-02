2a64b2d7a91feea3674c26598886aa6a8c2d3e0f
amaurywalbert <amaurywalbert@yahoo.com.br>
2017-09-01 21:38:55 -0300

    Atualizando arquivos de coleta

--- n9/n9_alters_friends_collect_v6.1_wait_APP4.py
+++ n9/n9_alters_friends_collect_v6.1_wait_APP4.py
@@ -63,7 +63,7 @@ def read_arq_bin(file):
 def save_error(user,reason):
 	agora = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M')				# Recupera o instante atual na forma AnoMesDiaHoraMinuto
 	error={}
-	with open(error_dir+"timeline_collect_wait.err", "a+") as outfile:								# Abre o arquivo para gravação no final do arquivo
+	with open(error_dir+"timeline_collect_wait.err.APP4", "a+") as outfile:								# Abre o arquivo para gravação no final do arquivo
 		error = {'user':user,'reason':str(reason) ,'date':agora}
 		outfile.write(json.dumps(error, cls=DateTimeEncoder, separators=(',', ':'))+"\n")
 	print error
