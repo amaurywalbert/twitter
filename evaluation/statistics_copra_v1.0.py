6c199a3ce4dde2755e31fe3f42e06c635b69ee99
amaurywalbert <amaurywalbert@yahoo.com.br>
2017-10-13 14:06:06 -0300

    Gerando grafos

--- evaluation/statistics_copra_v1.0.py
+++ evaluation/statistics_copra_v1.0.py
@@ -1,18 +1,14 @@
 	# -*- coding: latin1 -*-
 ################################################################################################
-import datetime, sys, time, json, os, os.path, shutil, time, struct, random
+import datetime, sys, time, json, os, os.path, shutil, time, struct, random, math
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import pylab
 import numpy as np
-import powerlaw
-import seaborn as sns
+from math import*
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
-import pandas as pd
-import math
-from math import*
 
 
 reload(sys)
@@ -33,42 +29,67 @@ sys.setdefaultencoding('utf-8')
 # Plota Gráficos dos dados...
 #
 ######################################################################################################################################################################
-def plot(output,data_overview,metric):
+def plot_single(output,data_overview,metric,title):
 	print ("\n##################################################\n")
 	print ("Gerando Gráfico...")
-	net = []
+	interaction = []
 	value = []
+	co_interaction = []
+	co_value = []	
 	for k, v in data_overview.iteritems():
 		if k == 'n1':
 			key = 'follow'
+			interaction.append(key)
+			value.append(round(v[metric], 3))			
 		elif k == 'n2':
 			key = 'retweets'			
+			interaction.append(key)
+			value.append(round(v[metric], 3))			
 		elif k == 'n3':
 			key = 'likes'
+			interaction.append(key)
+			value.append(round(v[metric], 3))			
 		elif k == 'n4':
 			key = 'mentions'
-		elif k == 'n5':
-			key = 'co-follow'
-		elif k == 'n6':
-			key = 'co-retweets'
-		elif k == 'n7':
-			key = 'co-likes'
-		elif k == 'n8':
-			key = 'co-mentions'
+			interaction.append(key)
+			value.append(round(v[metric], 3))			
 		elif k == 'n9':
 			key = 'followers'
+			interaction.append(key)
+			value.append(round(v[metric], 3))
+						
+		elif k == 'n5':
+			key = 'co-follow'
+			co_interaction.append(key)
+			co_value.append(round(v[metric], 3))			
+		elif k == 'n6':
+			key = 'co-retweets'
+			co_interaction.append(key)
+			co_value.append(round(v[metric], 3))			
+		elif k == 'n7':
+			key = 'co-likes'
+			co_interaction.append(key)
+			co_value.append(round(v[metric], 3))			
+		elif k == 'n8':
+			key = 'co-mentions'
+			co_interaction.append(key)
+			co_value.append(round(v[metric], 3))			
 		elif k == 'n10':
 			key = 'co-followers'
+			co_interaction.append(key)
+			co_value.append(round(v[metric], 3))
+			
+			
 		else:
-			key = ' '
 			print ("Valor incorreto para nome da rede-ego")
 			exit()																																						
-		net.append(key)
-		
-		value.append(round(v[metric], 3))
 
-	colors = ['lightblue', 'green', 'white', 'blue', 'red', 'lightblue', 'green', 'white', 'blue', 'red'] 	
-	data = [value,colors,net]
+	for item in co_value:
+		value.append(item)
+	for item in co_interaction:
+		interaction.append(item)
+	colors = ['lightblue', 'lightgreen', 'yellow', 'magenta', 'red', 'green', 'blue', 'cyan', 'magenta', 'black'] 	
+	data = [value,colors,interaction]
 	
 	print data 
 	
@@ -76,7 +97,7 @@ def plot(output,data_overview,metric):
 	barWidth = 0.50  # Largura da barra
 
 	_ax = plt.axes()  # Cria axes
-	_chartBars = plt.bar(xPositions, data[0], barWidth, color=data[1],yerr=0.1, align='center')  # Gera barras
+	_chartBars = plt.bar(xPositions, data[0], barWidth, color=data[1], align='center')  # Gera barras
 
 	for bars in _chartBars:
 		# text(x, y, s, fontdict=None, withdash=False, **kwargs)
@@ -85,11 +106,130 @@ def plot(output,data_overview,metric):
 	_ax.set_xticks(xPositions)
 	_ax.set_xticklabels(data[2])
 
+	plt.title(title)	
 	plt.xlabel('Rede-ego')
 	plt.ylabel(metric)
 	plt.legend(_chartBars, data[2])
+	
+	plt.savefig(output+str(title)+".png")
+	plt.close()
 
-	plt.show()
+######################################################################################################################################################################
+#
+# Plota Gráficos dos dados...
+#
+######################################################################################################################################################################
+def plot_full(data1,data2,data3,data4,metric,alg):
+	print ("\n##################################################\n")
+	print ("Gerando Gráfico...")
+
+	data_overview_full = [data1,data2,data3,data4]
+	dataset = {}
+	#i = 1										#armazenar dados de plotagem para dataset COM ego e comunidades COM singletons
+	#i = 2										#armazenar dados de plotagem para dataset COM ego e comunidades SEM singletons
+	#i = 3										#armazenar dados de plotagem para dataset SEM ego e comunidades COM singletons
+	#i = 4										#armazenar dados de plotagem para dataset SEM ego e comunidades SEM singletons
+	
+	i=0
+	for data_overview in data_overview_full:
+		i+=1
+		interaction = []
+		value = []
+		co_interaction = []
+		co_value = []	
+		for k, v in data_overview.iteritems():
+			if k == 'n1':
+				key = 'follow'
+				interaction.append(key)
+				value.append(round(v[metric], 3))			
+			elif k == 'n2':
+				key = 'retweets'			
+				interaction.append(key)
+				value.append(round(v[metric], 3))			
+			elif k == 'n3':
+				key = 'likes'
+				interaction.append(key)
+				value.append(round(v[metric], 3))			
+			elif k == 'n4':
+				key = 'mentions'
+				interaction.append(key)
+				value.append(round(v[metric], 3))			
+			elif k == 'n9':
+				key = 'followers'
+				interaction.append(key)
+				value.append(round(v[metric], 3))
+						
+			elif k == 'n5':
+				key = 'co-follow'
+#				co_interaction.append(key)
+#				co_value.append(round(v[metric], 3))			
+			elif k == 'n6':
+				key = 'co-retweets'
+				co_interaction.append(key)
+				co_value.append(round(v[metric], 3))			
+			elif k == 'n7':
+				key = 'co-likes'
+				co_interaction.append(key)
+				co_value.append(round(v[metric], 3))			
+			elif k == 'n8':
+				key = 'co-mentions'
+				co_interaction.append(key)
+				co_value.append(round(v[metric], 3))			
+			elif k == 'n10':
+				key = 'co-followers'
+#				co_interaction.append(key)
+#				co_value.append(round(v[metric], 3))
+			
+			else:
+				print ("Valor incorreto para nome da rede-ego")
+				exit()																																						
+
+		for item in co_value:
+			value.append(item)
+		for item in co_interaction:
+			interaction.append(item)
+		 	
+		data = [value,interaction]
+		dataset[i] = data
+
+	x = dataset[1][1]								# recebe os nomes das redes ego
+	n=len(x)
+	
+	y = np.array(dataset[1][0])								# recebe os valores para COM ego e comunidades COM singletons
+	z = np.array(dataset[2][0])								# recebe os valores para COM ego e comunidades SEM singletons
+	k = np.array(dataset[3][0])								# recebe os valores para SEM ego e comunidades COM singletons
+	w = np.array(dataset[4][0])								# recebe os valores para SEM ego e comunidades SEM singletons
+
+	ind=np.arange(n)
+	width=0.35
+
+	p1=plt.bar(ind-0.1,y,width,color="blue", label='Grafo COM ego')
+	p3=plt.bar(ind,k,width,color="lightblue", label='Grafo SEM ego')														## Invertido pra ficar mais facil a visualização no grafo
+	p2=plt.bar(ind+0.1,z,width,color="green", label='Grafo COM ego - Comunidade SEM singletons')					## Invertido pra ficar mais facil a visualização no grafo (lembrar de mudar o +0.1)
+	p4=plt.bar(ind+0.2,w,width,color="lightgreen", label='Grafo SEM ego - Comunidade SEM singletons')
+
+	plt.ylabel(metric)
+	plt.title("Avaliação das redes usando a métrica "+str(metric)+" e algoritmo "+str(alg))
+	
+	plt.xticks(ind+width/2,(x))
+	plt.legend(loc='best')	
+	plt.tight_layout()
+	plt.show()	
+#	plt.savefig(output+str(alg)+"_"+str(metric)+".png")
+	plt.close()
+################################################################################################  MANTER -- Dá pra exportar a tabela depois...
+
+	trace1 = go.Bar(x = dataset[1][1], y = dataset[1][0], name="Grafo COM ego", marker=dict(color='blue'))
+	trace2 = go.Bar(x = dataset[1][1], y = dataset[2][0], name="Grafo COM ego - Comunidade SEM singletons", marker=dict(color='green'))
+	trace3 = go.Bar(x = dataset[1][1], y = dataset[3][0], name="Grafo SEM ego", marker=dict(color='lightblue'))
+	trace4 = go.Bar(x = dataset[1][1], y = dataset[4][0], name="Grafo SEM ego - Comunidade SEM singletons", marker=dict(color='lightgreen'))
+	
+	data = [trace1, trace3,trace2,trace4]																						## Invertido pra ficar mais facil a visualização no grafo
+
+	layout = go.Layout(xaxis=dict(tickangle=-45),barmode='group',)
+	fig = go.Figure(data=data, layout=layout)
+
+	plotly.offline.plot(fig, filename=output+str(alg)+"_"+str(metric)+".html")
 
 
 ######################################################################################################################################################################
@@ -208,34 +348,46 @@ def copra(comm_data_dir,metric):
 #
 ######################################################################################################################################################################
 def instructions(metric):
-		comm_data_dir = str(source)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/full/"
-		output_dir = str(output)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/full/"
+
+	comm_data_dir = str(source)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/full/"
+	output_dir = str(output)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/full/"
 		
-		data,data_overview = copra(comm_data_dir,metric)
-		save_data(output_dir,data,data_overview)		
-		plot(output,data_overview,metric)	
+	data,data_overview = copra(comm_data_dir,metric)
+	save_data(output_dir,data,data_overview)		
+	plot_single(output,data_overview,metric,title='Graphs with ego - Communities with singletons')	
+	data1 = data_overview
+################################################################################################
 
-		comm_data_dir = str(source)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
-		output_dir = str(output)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
+	comm_data_dir = str(source)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
+	output_dir = str(output)+"graphs_with_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
 
-		data,data_overview = copra(comm_data_dir,metric)
-		save_data(output_dir,data,data_overview)		
-		plot(output,data_overview,metric)	
+	data,data_overview = copra(comm_data_dir,metric)
+	save_data(output_dir,data,data_overview)		
+	plot_single(output,data_overview,metric,title='Graphs with ego - Communities without singletons')	
+	data2 = data_overview
+################################################################################################
 
-		comm_data_dir = str(source)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/full/"
-		output_dir = str(output)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/full/"
+	comm_data_dir = str(source)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/full/"
+	output_dir = str(output)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/full/"
 
-		data,data_overview = copra(comm_data_dir,metric)
-		save_data(output_dir,data,data_overview)		
-		plot(output,data_overview,metric)		
+	data,data_overview = copra(comm_data_dir,metric)
+	save_data(output_dir,data,data_overview)		
+	plot_single(output,data_overview,metric,title='Graphs without ego - Communities with singletons')
+	data3 = data_overview
+################################################################################################
 
-		comm_data_dir = str(source)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
-		output_dir = str(output)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
+	comm_data_dir = str(source)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
+	output_dir = str(output)+"graphs_without_ego/"+algorithm+"/"+str(metric)+"/without_singletons/"
 
-		data,data_overview = copra(comm_data_dir,metric)
-		save_data(output_dir,data,data_overview)		
-		plot(output,data_overview,metric)	
+	data,data_overview = copra(comm_data_dir,metric)
+	save_data(output_dir,data,data_overview)		
+	plot_single(output,data_overview,metric,title='Graphs without ego - Communities without singletons')	
+	data4 = data_overview
 
+################################################################################################
+	alg = "copra"
+	plot_full(data1,data2,data3,data4,metric,alg)		
+		
 ######################################################################################################################################################################
 #
 # Método principal do programa.
