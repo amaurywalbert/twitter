b22d7c95cae7d10d89a5e73efbcf609184236557
Amaury Carvalho <amaurywalbert@yahoo.com.br>
2017-10-23 15:29:48 -0200

    Calcular clustering Coefficient

---
+++ net_structure/clustering_coefficient.py
@@ -0,0 +1,164 @@
+# -*- coding: latin1 -*-
+################################################################################################
+#	
+#
+import snap, datetime, sys, time, json, os, os.path, shutil, time, random, math
+import numpy as np
+from math import*
+# Script auxiliar para cálculos matemáticos que deve estar no mesmo diretório deste aqui.
+import calc
+# Script auxiliar para gerar histogramas
+import histogram
+import networkx as nx
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+######################################################################################################################################################################
+##		Status - Versão 1 - Script para gerar coeficiente de clustering de cada rede e a média de todas as redes por ego
+## 
+######################################################################################################################################################################
+
+
+######################################################################################################################################################################
+#
+# Armazenar as propriedades do dataset
+#
+######################################################################################################################################################################
+def net_structure(dataset_dir,output_dir,net,IsDir, weight):
+	print("\n######################################################################\n")
+	print ("Dataset clustering coefficient - " +str(dataset_dir))
+											
+	cf = []																										# Média dos coeficientes de clusterings por rede-ego
+	n = []																										# vetor com número de vértices para cada rede-ego																									
+	e = []																										# vetor com número de arestas para cada rede-ego
+	i = 0
+
+
+	for file in os.listdir(dataset_dir):
+		i+=1 
+		print ("Calculando propriedades para o ego %d..." % (i))
+		if IsDir is True:
+			G = snap.LoadEdgeList(snap.PNGraph, dataset_dir+file, 0, 1)					   # load from a text file - pode exigir um separador.: snap.LoadEdgeList(snap.PNGraph, file, 0, 1, '\t')
+		else:
+			G = snap.LoadEdgeList(snap.PUNGraph, dataset_dir+file, 0, 1)					# load from a text file - pode exigir um separador.: snap.LoadEdgeList(snap.PNGraph, file, 0, 1, '\t')
+#####################################################################################		
+
+		n.append(G.GetNodes())																		# Numero de vertices
+		e.append(G.GetEdges())																		# Numero de arestas
+		n_nodes = G.GetNodes()	
+		n_edges = G.GetEdges()
+		
+#####################################################################################
+
+		NIdCCfH = snap.TIntFltH()
+		snap.GetNodeClustCf(G, NIdCCfH)
+		print ("OK. Estou aqui...")
+		_cf = []
+		for item in NIdCCfH:
+			print item, NIdCCfH[item]
+			_cf.append(NIdCCfH[item])																# Clusterinf Coefficient
+		result = calc.calcular(_cf)
+		cf.append(result['media'])
+#####################################################################################
+	
+	CF = calc.calcular_full(cf)
+	
+	overview = {}
+	overview['ClusteringCoefficient'] = CF
+	
+	with open(str(output_dir)+str(net)+"_clustering_coef.json", 'w') as f:
+		f.write(json.dumps(overview))
+	
+	with open(str(output_dir)+str(net)+"_net_struct.txt", 'w') as f:
+		f.write("\n######################################################################\n")	
+		f.write ("Clustering Coef: Média: %5.3f -- Var:%5.3f -- Des. Padrão: %5.3f \n"% (CF['media'],CF['variancia'],CF['desvio_padrao']))
+		f.write("\n######################################################################\n")
+
+print("\n######################################################################\n")
+
+######################################################################################################################################################################
+######################################################################################################################################################################
+#
+# Método principal do programa. 
+#
+######################################################################################################################################################################
+######################################################################################################################################################################
+
+def main():
+	os.system('clear')
+	print "################################################################################"
+	print"																											"
+	print" Script para cálculo do coeficiente de agrupamento do dataset (rede-ego)							"
+	print"																											"
+	print"#################################################################################"
+	print
+	print"  1 - Follow"
+	print"  9 - Follwowers"
+	print"  2 - Retweets"
+	print"  3 - Likes"
+	print"  3 - Mentions"
+	
+	print " "
+	print"  5 - Co-Follow"
+	print" 10 - Co-Followers"				
+	print"  6 - Co-Retweets"
+	print"  7 - Co-Likes"
+	print"  8 - Co-Mentions"
+			
+	print
+	op = int(raw_input("Escolha uma opção acima: "))
+
+
+	if op in (5,6,7,8,10):																						# Testar se é um grafo direcionado ou não
+		isdir = False
+	elif op in (1,2,3,4,9):
+		isdir = True 
+	else:
+		print("Opção inválida! Saindo...")
+		sys.exit()
+
+	if op == 1 or op == 9:																						# Testar se é um grafo direcionado ou não
+		weight = False
+	else:
+		weight = True
+######################################################################
+	
+	net = "n"+str(op)	
+
+######################################################################		
+######################################################################
+	dataset_dir = "/home/amaury/graphs/"+str(net)+"/graphs_with_ego/"								############### Arquivo contendo arquivos com a lista de arestas das redes-ego
+
+	if not os.path.isdir(dataset_dir):
+		print("Diretório dos grafos não encontrado: "+str(dataset_dir))
+	else:
+		output_dir = "/home/amaury/Dropbox/clustering_coefficient/graphs_with_ego/"
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir)
+		net_structure(dataset_dir,output_dir,net,isdir,weight)													# Inicia os cálculos...				
+######################################################################		
+######################################################################
+	dataset_dir2 = "/home/amaury/graphs/"+str(net)+"/graphs_without_ego/"						############### Arquivo contendo arquivos com a lista de arestas das redes-ego
+	if not os.path.isdir(dataset_dir2):
+		print("Diretório dos grafos não encontrado: "+str(dataset_dir2))
+	else:
+		output_dir2 = "/home/amaury/Dropbox/clustering_coefficient/graphs_without_ego/"
+		if not os.path.exists(output_dir2):
+			os.makedirs(output_dir2)
+		net_structure(dataset_dir2,output_dir2,net,isdir,weight)												# Inicia os cálculos...	
+######################################################################
+######################################################################		
+
+	print("\n######################################################################\n")
+	print("Script finalizado!")
+	print("\n######################################################################\n")
+
+######################################################################################################################################################################
+#
+# INÍCIO DO PROGRAMA
+#
+######################################################################################################################################################################
+
+
+#Executa o método main
+if __name__ == "__main__": main()
