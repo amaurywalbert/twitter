05a0ab0a78a1fbc8dfe5cfc1b02390254973889c
Amaury Carvalho <amaurywalbert@yahoo.com.br>
2017-11-09 22:38:21 -0200

    HashMap

--- hashmap/hashmap_dataset_v1.0.py
+++ hashmap/hashmap_dataset_v1.0.py
@@ -1,9 +1,6 @@
 # -*- coding: latin1 -*-
 ################################################################################################
-import datetime, sys, time, json, os, os.path, shutil, time, struct, random
-import subprocess
-
-
+import datetime, sys, time, json, os, os.path, shutil, time
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -22,12 +19,13 @@ sys.setdefaultencoding('utf-8')
 
 ######################################################################################################################################################################
 #
-# ADD ground_truth and communitites ao hashmap 
+# ADD ground_truth and communitites ao hashmap
+#
+#		Adiciona o id do usuário como chave de um dicionário e vincula essa chave a um numero dado pela numero sequencial correspondente à aparição do usuário no arquivo. 
 #
 ######################################################################################################################################################################
-def add_hashmap(i,hashmap,f):
+def add_hashmap(i,hashmap,f):		
 	for line in f:
-		print line
 		alters = line.split(' ')
 		for alter in alters:
 			if alter != "\n":
@@ -38,8 +36,10 @@ def add_hashmap(i,hashmap,f):
 #
 # ADD egonet ao hashmap 
 #
+#		Para cada par de usuários representando cada terminal de uma aresta, adiciona o id do usuário como chave de um dicionário e vincula essa chave a um numero dado pela numero sequencial correspondente à aparição do usuário no arquivo.
+#
 ######################################################################################################################################################################
-def add_hashmap_egonet(i,hashmap,f):
+def add_hashmap_egonet(i,hashmap,f):	
 	for line in f:
 		alters = line.split(' ')
 		if alters[0] != "\n":
@@ -53,6 +53,8 @@ def add_hashmap_egonet(i,hashmap,f):
 ######################################################################################################################################################################
 #
 # SAVE ground truth and communities - hashmap 
+#
+#		Percorre o arquivo e verifica o valor vinculado a ele consultando o HashMAP. Escreve o novo valor no mesmo formato do arquivo de entrada.
 #
 ######################################################################################################################################################################
 def save_hashmap(i,hashmap,f,g,test):
@@ -68,6 +70,8 @@ def save_hashmap(i,hashmap,f,g,test):
 #
 # SAVE egonet - hashmap 
 #
+##		Percorre o arquivo e verifica o valor vinculado a ele consultando o HashMAP. Escreve o novo valor no mesmo formato do arquivo de entrada, mantendo o valor do peso da aresta inalterado, caso houver.
+#
 ######################################################################################################################################################################
 def save_hashmap_egonet(i,hashmap,f,g,test):
 	for line in f:
@@ -75,10 +79,9 @@ def save_hashmap_egonet(i,hashmap,f,g,test):
 		alters[0] = long(alters[0])
 		alters[1] = long(alters[1])
 		if len(alters) > 2:
-			g.write(str(hashmap[alters[0]])+" "+str(hashmap[alters[1]])+str(alters[2])+"\n")
+			g.write(str(hashmap[alters[0]])+" "+str(hashmap[alters[1]])+" "+str(alters[2]))
 		else:
-			g.write(str(hashmap[alters[0]])+" "+str(hashmap[alters[1]])+"\n")	
-	
+			g.write(str(hashmap[alters[0]])+" "+str(hashmap[alters[1]]))
 
 ######################################################################################################################################################################
 #
@@ -94,18 +97,19 @@ def main():
 	print" 			Converte longint para int usando HashMap											"
 	print"																											"
 	print"#################################################################################"	
-	
+	j = 0
 	for file in os.listdir(ground_truth_source+str("full/")):				# Iniciar o ego
+		j+=1
 		ego = file.split(".txt")
 		ego = ego[0]
 		hashmap = {}
 		i=0
 ######################################################################################################################
 ################################################################## GROUND TRUTH
-
 		for net_type in os.listdir(ground_truth_source):
 			if net_type in("full","without_singletons"):
 				if os.path.isdir(ground_truth_source+"/"+net_type):
+
 					with open(ground_truth_source+net_type+"/"+file, 'r') as f:		# SOURCE - Para arquivos no diretório ground_truth/...
 						i,hashmap = add_hashmap(i,hashmap,f)
 
@@ -116,10 +120,11 @@ def main():
 				nets.append(diretorio)
 		
 		for net in nets:
-			for diretorio in os.listdir(egonet_source):
+			for diretorio in os.listdir(egonet_source+net):
 				if diretorio in ("graphs_with_ego","graphs_without_ego"):
-					if os.path.isdir(communities_source+diretorio):
-						with open(egonet_source+diretorio+"/"+str(ego)+".edge_list", 'r') as f:		# SOURCE - Para arquivos no diretório ego/net/nx/graphs_with_ego/
+					if os.path.isdir(egonet_source+net+"/"+diretorio):
+
+						with open(egonet_source+net+"/"+diretorio+"/"+str(ego)+".edge_list", 'r') as f:		# SOURCE - Para arquivos no diretório ego/net/nx/graphs_with_ego/
 							i,hashmap = add_hashmap_egonet(i,hashmap,f)
 
 ################################################################## COMMUNITIES
@@ -136,6 +141,7 @@ def main():
 												for threshold in os.listdir(communities_source+diretorio+"/"+alg+"/"+net_type+"/"+net):
 													if os.path.isdir(communities_source+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold):
 														if os.path.isfile(communities_source+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold+"/"+file):
+
 															with open(communities_source+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold+"/"+file, 'r') as f:		# SOURCE - Para arquivos no communities_source/graphs_withX_ego/"alg"/"net_type"/"net"
 																i,hashmap = add_hashmap(i,hashmap,f)
 
@@ -150,22 +156,26 @@ def main():
 				if os.path.isdir(ground_truth_source+"/"+net_type):
 
 					with open(ground_truth_source+net_type+"/"+file, 'r') as f:		# SOURCE - Para arquivos no diretório ground_truth/...						
+
 						if not os.path.exists(ground_truth_output+net_type+"/"):
 							os.makedirs(ground_truth_output+net_type+"/")
+
 						with open(ground_truth_output+net_type+"/"+file, 'w') as g:
 							save_hashmap(i,hashmap,f,g,test)
 
 
 ################################################################## EGONETS
 		for net in nets:
-			for diretorio in os.listdir(egonet_source):
+			for diretorio in os.listdir(egonet_source+net):
 				if diretorio in ("graphs_with_ego","graphs_without_ego"):
-					if os.path.isdir(communities_source+diretorio):
+					if os.path.isdir(egonet_source+net+"/"+diretorio):
+						
+						with open(egonet_source+net+"/"+diretorio+"/"+str(ego)+".edge_list", 'r') as f:		# SOURCE - Para arquivos no diretório ego/net/nx/graphs_with_ego/
+							
+							if not os.path.exists(egonet_output+net+"/"+diretorio+"/"):
+								os.makedirs(egonet_output+net+"/"+diretorio+"/")
 
-						with open(egonet_source+diretorio+"/"+str(ego)+".edge_list", 'r') as f:		# SOURCE - Para arquivos no diretório ego/net/nx/graphs_with_ego/
-							if not os.path.exists(egonet_output+diretorio+"/"):
-								os.makedirs(egonet_output+diretorio+"/")
-							with open(egonet_output+diretorio+"/"+str(ego)+".edge_list", 'w') as g:		# SOURCE - Para arquivos no diretório ego/net/nx/graphs_with_ego/													
+							with open(egonet_output+net+"/"+diretorio+"/"+str(ego)+".edge_list", 'w') as g:		# SOURCE - Para arquivos no diretório ego/net/nx/graphs_with_ego/													
 								save_hashmap_egonet(i,hashmap,f,g,test)
 
 
@@ -185,15 +195,18 @@ def main():
 														if os.path.isfile(communities_source+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold+"/"+file):
 															
 															with open(communities_source+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold+"/"+file, 'r') as f:		# SOURCE - Para arquivos no communities_source/graphs_withX_ego/"alg"/"net_type"/"net"
+
 																if not os.path.exists(communities_output+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold+"/"):
 																	os.makedirs(communities_output+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold+"/")
+
 																with open(communities_output+diretorio+"/"+alg+"/"+net_type+"/"+net+"/"+threshold+"/"+file, 'w') as g:
 																	save_hashmap(i,hashmap,f,g,test)																
 ######################################################################################################################		
-		print ego, test, len(test), len(hashmap)
-#		time.sleep(5)
-		print 						
+
+		print ("Convertendo arquivos do ego "+str(j)+": "+str(ego)) 						
+
 ######################################################################################################################
+
 	print
 	print("######################################################################")
 	print("Script finalizado!")
@@ -206,7 +219,7 @@ def main():
 #
 ######################################################################################################################################################################
 ground_truth_source = "/home/amaury/dataset/ground_truth/lists_users_TXT/"
-ground_truth_output = "/home/amaury/dataset/ground_truth_hashmap/"
+ground_truth_output = "/home/amaury/dataset/ground_truth/lists_users_TXT_hashmap/"
 
 egonet_source = "/home/amaury/graphs/"
 egonet_output = "/home/amaury/graphs_hashmap/"
