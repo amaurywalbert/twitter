30e17b2bd87e5ea9b56ed0187796afb5e243f3a9
Amaury Carvalho <amaurywalbert@yahoo.com.br>
2017-11-19 01:18:07 -0200

    Atualizando hashmap

--- hashmap/hashmap_dataset_v1.0.py
+++ hashmap/hashmap_dataset_v1.0.py
@@ -90,6 +90,7 @@ def save_hashmap_communities(i,hashmap,f,g,ego):
 						g.write(str(hashmap[alter])+" ")									# Escreve os ids das Listas separadas por espaço
 				except Exception as e:		
 					with open(error_dir+ego+".txt", "a+") as err_file:
+						print +str(e)+" - alter: "+str(alter)
 						err_file.write("\n"+str(e)+" - alter: "+str(alter)+"\n")
 		g.write("\n")		
 	return (i,hashmap)
