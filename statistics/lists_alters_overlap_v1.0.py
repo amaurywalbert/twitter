09c14a922240f197f7b077dca68c829d82733a09
Amaury Carvalho <amaurywalbert@yahoo.com.br>
2018-05-19 18:52:49 -0300

    Atualizando lists properties

--- statistics/lists_alters_overlap_v1.0.py
+++ statistics/lists_alters_overlap_v1.0.py
@@ -73,7 +73,9 @@ def get_alters_set(ego_id,file):
 					ranking.append(tuple)
 
 	ranking = sorted(ranking, key=lambda x: (x[1], -x[0]), reverse=True) 		#Ordena uma tupla decrescente (id,weight)). Em caso de empate ordena crecente pelo id os empatados
-	for i in range(10):
+	
+	top_k = []						
+	for i in range(10):															# Cria um sub-ranking com apenas os top-k elementos com os quais o ego mais interagiu nessa camada.
 		top_k.append(ranking[i])
 	
 	print ranking	
