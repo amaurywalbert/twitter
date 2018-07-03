dc40a43fd7b1325aa01fb1bfcdf3f33bb171dbcf
Amaury Carvalho <amaurywalbert@yahoo.com.br>
2018-07-03 14:59:53 -0300

    Atualizando calculo do Small World - SNAP

--- net_structure/multilayer/unweighted_directed/hashmap_merge_small_worlds_results_v1.py
+++ net_structure/multilayer/unweighted_directed/hashmap_merge_small_worlds_results_v1.py
@@ -113,13 +113,13 @@ print"  3 - NetworkX - UNDirected"
 print
 op = int(raw_input("Escolha a biblioteca utilizada para calcular a métrica S : "))
 	
-if op == 1:
+if op == int(1):
 	source_dir = "/home/amaury/Dropbox/net_structure_hashmap/multilayer/graphs_with_ego/unweighted_directed/json/small_world/"
 	output_dir = source_dir
-elif op == 2: 
+elif op == int(2): 
 	source_dir = "/home/amaury/Dropbox/net_structure_hashmap/multilayer/graphs_with_ego/unweighted_directed/json/small_world_snap/"
 	output_dir = source_dir
-if op == 3:
+if op == int(3):
 	source_dir = "/home/amaury/Dropbox/net_structure_hashmap/multilayer/graphs_with_ego/unweighted_undirected/json/small_world/"
 	output_dir = source_dir
 else:
